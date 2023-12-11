37d5b5d72dca9827aa28fc749d8b932c7f7af6b3
Aldo <aldoagunin@gmail.com>
2023-12-11 08:50:27 -0300

    limpiado editar_producto.js

--- js/editar_producto.js
+++ js/editar_producto.js
@@ -30,7 +30,8 @@ function modificar() {
             alert("Registro modificado")
 
             //Puedes utilizar window.location.href para obtener la URL actual, redirigir a otras páginas
-           window.location.href = "../templates/tabla_productos.html";
+            window.location.href = "../templates/tabla_productos.html";
+           
           
         })
         .catch(err => {
@@ -38,45 +39,4 @@ function modificar() {
             console.error(err);
             alert("Error al Modificar")
         })      
-=======
-function modificar() {
-    let id = document.getElementById("id").value
-    let nombre_ingresado = document.getElementById("nombre").value
-    let precio_ingresado = document.getElementById("precio").value 
-    let stock_ingresado = document.getElementById("stock").value 
-    let imagen_ingresada = document.getElementById("imagen").value 
-
-    let datos = {
-        nombre: nombre_ingresado,
-        precio:precio_ingresado,
-        stock:stock_ingresado,
-        imagen:imagen_ingresada
     }
-
-    console.log(datos);
-
-    let url = "http://localhost:5000/update/"+id
-    var options = {
-        body: JSON.stringify(datos),
-        method: 'PUT',
-        
-        headers: { 'Content-Type': 'application/json' },
-        // el navegador seguirá automáticamente las redirecciones y
-        // devolverá el recurso final al que se ha redirigido.
-        redirect: 'follow'
-    }
-    fetch(url, options)
-        .then(function () {
-            console.log("modificado")
-            alert("Registro modificado")
-
-            //Puedes utilizar window.location.href para obtener la URL actual, redirigir a otras páginas
-           window.location.href = "../templates/tabla_productos.html";
-          
-        })
-        .catch(err => {
-            this.error = true
-            console.error(err);
-            alert("Error al Modificar")
-        })      
-}
