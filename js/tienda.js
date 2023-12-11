8dfe7dec49db00429361b4758060c7c117add303
Aldo <aldoagunin@gmail.com>
2023-12-11 06:17:07 -0300

    arreglada tienda

--- js/tienda.js
+++ js/tienda.js
@@ -1,6 +1,9 @@
 let products = [];
 let total = 0;
 
+let carrito =[];
+let importe = 0;
+
 function add(product, price) {
     console.log(product, price);
     products.push(product);
@@ -10,4 +13,13 @@ function add(product, price) {
 
 function pay() {
     window.alert(products.join(", \n"));
+}
+
+function agregarAlCarrito(id, nombre, precio) {
+    // Aquí puedes realizar las acciones que desees con los datos del producto
+    console.log(`Agregado al carrito: ID ${id}, Nombre ${nombre}, Precio ${precio}`);
+    carrito.push(nombre);
+    importe = importe + precio;
+    document.getElementById("checkout").innerHTML = `Pagar $${importe}`
+
 }
